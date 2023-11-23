81f8b127ace7206cbdb0a2d24e2bb37c2f745016
Bianca Pereira de Almeida <bpereiraalmeida@gmail.com>
2023-11-22 22:36:33 -0300

    Ajustando referência de projeto e criação de project teste

---
+++ CleanArch.Test/Application/Services/MoviesServiceTest.cs
@@ -0,0 +1,11 @@
+namespace CleanArch.Test.Application.Services
+{
+    public class MoviesServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            /// Em construção
+        }
+    }
+}
